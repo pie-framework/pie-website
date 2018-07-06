ed445fe760f257ee8adf25b6eeff2cd3ded48f15
evaneus <evan.eustace@gmail.com>
2018-07-06 09:12:49 -0400

    set up for localization, add title and logo

--- site/.vuepress/config.js
+++ site/.vuepress/config.js
@@ -1,35 +1,43 @@
 module.exports = {
+  title: 'PIE Framework',
   themeConfig: {
-    nav: [
-      { text: 'Docs', link: '/docs/introduction' },
-      { text: 'Examples', link: '/examples' },
-      { text: 'Github', link: 'https://github.com/pie-framework' }
-    ],
-    sidebar: [
-      '/docs/introduction',
-      {
-        title: 'Using Pie Content',
-        collapsable: false,
-        children: [
-          '/docs/using/quick-start',
-          '/docs/using/defining-items',
-          '/docs/using/packaging-items',
-          '/docs/using/rendering-items',
-          '/docs/using/pie-player-api'
-        ]
-      },
-      {
-        title: 'PIE Development',
-        collapsable: false,
-        children: [
-          '/docs/developing/summary',
-          '/docs/developing/custom-element',
-          '/docs/developing/controller',
-          '/docs/developing/packaging',
-          '/docs/developing/environment',
-          '/docs/developing/tutorial/development-tutorial'
+    logo: '/pie-logo.svg',
+    siteTitle: 'PIE framework',
+    locales: {
+      '/': {
+        title: 'PIE framework',
+        nav: [
+          { text: 'Docs', link: '/docs/introduction' },
+          { text: 'Examples', link: '/examples' },
+          { text: 'Github', link: 'https://github.com/pie-framework' }
+        ],
+        sidebar: [
+          '/docs/introduction',
+          {
+            title: 'Using Pie Content',
+            collapsable: false,
+            children: [
+              '/docs/using/quick-start',
+              '/docs/using/defining-items',
+              '/docs/using/packaging-items',
+              '/docs/using/rendering-items',
+              '/docs/using/pie-player-api'
+            ]
+          },
+          {
+            title: 'PIE Development',
+            collapsable: false,
+            children: [
+              '/docs/developing/summary',
+              '/docs/developing/custom-element',
+              '/docs/developing/controller',
+              '/docs/developing/packaging',
+              '/docs/developing/environment',
+              '/docs/developing/tutorial/development-tutorial'
+            ]
+          }
         ]
       }
-    ]
+    }
   }
 };
