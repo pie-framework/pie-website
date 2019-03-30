748d0086f4a7dd9e307d8e44c04bb0b1e9587c03
lakatosandrei <lakatosandrei@gmail.com>
2019-03-30 11:31:21 +0200

    feat(circleci): telling git who am i

--- scripts/commitChanges.js
+++ scripts/commitChanges.js
@@ -4,9 +4,10 @@ const exec = util.promisify(require('child_process').exec);
 async function pushChanges() {
   const addCommand = 'git add site/changes site/.vuepress';
   const commitCommand = 'git commit -m "[ci skip] update the changelog and deploy-info.json files"';
+  const configSet = `git config user.email "lakatosandrei@gmail.com" && git config user.name "lakatosandrei"`;
   const pushCommand = 'git push -q https://${GITHUB_TOKEN}@github.com/pie-framework/pie-website.git master';
 
-  const { stdout, stderr } = await exec(`${addCommand} && ${commitCommand} && ${pushCommand}`);
+  const { stdout, stderr } = await exec(`${configSet} && ${addCommand} && ${commitCommand} && ${pushCommand}`);
 
   console.log('stdout:', stdout);
   console.log('stderr:', stderr);
