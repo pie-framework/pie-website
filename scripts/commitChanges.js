c442c5b352003d9e08bedbc61067d11c8045c630
lakatosandrei <lakatosandrei@gmail.com>
2019-03-30 11:25:53 +0200

    feat(circleci): used the github token for the push

--- scripts/commitChanges.js
+++ scripts/commitChanges.js
@@ -4,7 +4,7 @@ const exec = util.promisify(require('child_process').exec);
 async function pushChanges() {
   const addCommand = 'git add site/changes site/.vuepress';
   const commitCommand = 'git commit -m "[ci skip] update the changelog and deploy-info.json files"';
-  const pushCommand = 'git push origin master';
+  const pushCommand = 'git push -q https://${GITHUB_TOKEN}@github.com/pie-framework/pie-website.git master';
 
   const { stdout, stderr } = await exec(`${addCommand} && ${commitCommand} && ${pushCommand}`);
 
