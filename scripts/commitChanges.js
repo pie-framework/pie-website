cc4fbd65b38505466902777538793528bcffc01d
lakatosandrei <lakatosandrei@gmail.com>
2019-04-09 18:44:29 +0300

    chore(general): Commented the end date criteria for changelogs and removed faulty logic

--- scripts/commitChanges.js
+++ scripts/commitChanges.js
@@ -2,14 +2,13 @@ const util = require('util');
 const exec = util.promisify(require('child_process').exec);
 
 async function pushChanges() {
-  const pullingCommand = 'git pull origin master';
   const addCommand = 'git add site/changes site/.vuepress';
   const commitCommand = 'git commit -m "[ci skip] update the changelog and deploy-info.json files"';
   const configSet = `git config user.email "lakatosandrei@gmail.com" && git config user.name "lakatosandrei"`;
   const pushCommand = 'git push -q https://${GITHUB_TOKEN}@github.com/pie-framework/pie-website.git master';
 
   try {
-    const { stdout, stderr } = await exec(`${pullingCommand} && ${configSet} && ${addCommand} && ${commitCommand} && ${pushCommand}`);
+    const { stdout, stderr } = await exec(`${configSet} && ${addCommand} && ${commitCommand} && ${pushCommand}`);
 
     console.log('stdout:', stdout);
     console.log('stderr:', stderr);
