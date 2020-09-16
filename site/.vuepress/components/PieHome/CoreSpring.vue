087c9834a9ad1dca02c8342583ad17d1ac8b1e90
evaneus <evan.eustace@gmail.com>
2020-09-16 15:07:27 -0400

    chore: disable docs until updated (#24)

--- site/.vuepress/components/PieHome/CoreSpring.vue
+++ site/.vuepress/components/PieHome/CoreSpring.vue
@@ -9,14 +9,10 @@
                 </div>
                 <div class="text-container">
                         <span class="first-text">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-                            dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                            ex ea commodo consequat.
+                            PIE Framework was originally developed by the not for profit organization CoreSrpring Inc
                         </span>
                     <span class="second-text">
-                            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore
-                            eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
-                            deserunt mollit anim id est laborum.
+                            Todo
                     </span>
                 </div>
                 <InfoLink></InfoLink>
