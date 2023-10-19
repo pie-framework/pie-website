2ef36ec51131e2110202d71e620f4f4155ec47bf
lakatosandrei <lakatosandrei@gmail.com>
2023-10-19 12:20:45 -0400

    Update routes and themeconfig to go direct to examples PD-3242

--- site/.vuepress/enhanceApp.js
+++ site/.vuepress/enhanceApp.js
@@ -1,5 +1,7 @@
 export default ({ router }) => {
   router.addRoutes([
+    { path: '/', redirect: '/examples/pie-categorize.html'},
+    { path: '/examples-next', redirect: '/examples-next/pie-categorize.html'},
     { path: '/changes', redirect: '/changes/introduction.html' },
     { path: '/changelogs', redirect: '/changelogs/introduction.html' },
   ])
