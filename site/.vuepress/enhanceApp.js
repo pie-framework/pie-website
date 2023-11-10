836a7f4bc6e98628fb200db29b523139a27a332d
lakatosandrei <lakatosandrei@gmail.com>
2023-11-10 09:52:35 -0500

    Update routes PD-3268

--- site/.vuepress/enhanceApp.js
+++ site/.vuepress/enhanceApp.js
@@ -1,9 +1,7 @@
 export default ({ router }) => {
   router.addRoute(
     {path: '/', redirect: '/'}, 
-    { path: '/examples', redirect: '/'}, 
-    { path: '/examples-next', redirect: '/'},
-    { path: '/examples-next/', redirect: '/'},
+    { path: '/examples', redirect: '/'},
     { path: '/changes', redirect: '/changes/introduction.html' },
     { path: '/changelogs', redirect: '/changelogs/introduction.html' }
   )
