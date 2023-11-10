93c0d36938b709e102660ba54b383500c1740e59
lakatosandrei <lakatosandrei@gmail.com>
2023-11-10 11:33:50 -0500

    Clean up and add a route to enhanceApp PD-3268

--- site/.vuepress/enhanceApp.js
+++ site/.vuepress/enhanceApp.js
@@ -1,5 +1,6 @@
 export default ({ router }) => {
   router.addRoute(
+    { path: '/examples', redirect: '/'},
     { path: '/changes', redirect: '/changes/introduction.html' },
     { path: '/changelogs', redirect: '/changelogs/introduction.html' }
   )
